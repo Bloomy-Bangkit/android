d8837574d31947648b170231bf8c9e99f6d0051a
Sandhi Karunia Sugihartana <sandhi372@gmail.com>
2023-11-28 22:35:12 +0700

    Create SplashActivity.kt

---
+++ app/src/main/java/com/dicoding/bloomy/ui/activity/SplashActivity.kt
@@ -0,0 +1,26 @@
+package com.dicoding.bloomy.ui.activity
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.dicoding.bloomy.R
+
+@SuppressLint("CustomSplashScreen")
+class SplashActivity : AppCompatActivity() {
+
+    private val splashDuration: Long = 3000
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val homeIntent = Intent(this, HomeActivity::class.java)
+            startActivity(homeIntent)
+            finish()
+        }, splashDuration)
+    }
+}
