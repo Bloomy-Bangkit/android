4d87cd49b8f7a0bdd5475d81c2f5fd0b5ccfa659
Sandhi Karunia Sugihartana <sandhi372@gmail.com>
2023-11-28 13:56:18 +0700

    Create MainActivity.kt

---
+++ app/src/main/java/com/dicoding/bloomy/ui/activity/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.dicoding.bloomy.ui.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.dicoding.bloomy.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
