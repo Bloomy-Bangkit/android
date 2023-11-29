d19615512cfea01e28e5600e7ca7f09ef6c2064d
Sandhi Karunia Sugihartana <sandhi372@gmail.com>
2023-11-29 22:31:36 +0700

    Create ProfileActivity.kt

---
+++ app/src/main/java/com/capstone/bloomy/ui/activity/ProfileActivity.kt
@@ -0,0 +1,20 @@
+package com.capstone.bloomy.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.capstone.bloomy.R
+import com.capstone.bloomy.databinding.ActivityProfileBinding
+
+class ProfileActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityProfileBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityProfileBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.materialToolBar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+}
