0a4701f91dc29bba6d13ce36c3e312fb95f01312
Sandhi Karunia Sugihartana <sandhi372@gmail.com>
2023-11-29 22:31:40 +0700

    Update SignInActivity.kt

--- app/src/main/java/com/capstone/bloomy/ui/activity/SignInActivity.kt
+++ app/src/main/java/com/capstone/bloomy/ui/activity/SignInActivity.kt
@@ -1,12 +1,30 @@
 package com.capstone.bloomy.ui.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.capstone.bloomy.R
+import com.capstone.bloomy.databinding.ActivitySignInBinding
 
 class SignInActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySignInBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_in)
+        binding = ActivitySignInBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnSignIn.setOnClickListener {
+            val profileIntent = Intent(this, ProfileActivity::class.java)
+            startActivity(profileIntent)
+            finish()
+        }
+
+        binding.btnSignUp.setOnClickListener {
+            val signUpIntent = Intent(this, SignUpActivity::class.java)
+            startActivity(signUpIntent)
+            finish()
+        }
     }
 }
