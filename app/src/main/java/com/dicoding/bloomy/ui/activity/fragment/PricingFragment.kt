acf94f82e8f1589d74c3210858ba3cc977f3a5bd
VBeatDead <77430117+VBeatDead@users.noreply.github.com>
2023-12-03 13:24:21 +0700

    Add Activity
    Add Activity detail transaction, history transaction, edit transaction, fish grading, fish pricing dll

---
+++ app/src/main/java/com/dicoding/bloomy/ui/activity/fragment/PricingFragment.kt
@@ -0,0 +1,18 @@
+package com.dicoding.bloomy.ui.activity.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.dicoding.bloomy.R
+
+class PricingFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.activity_fish_pricing, container, false)
+    }
+}
