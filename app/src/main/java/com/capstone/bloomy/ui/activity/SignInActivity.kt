5d0326f08ab728bb93e4b6c0dfb4b71852ef53f7
Sandhi Karunia Sugihartana <sandhi372@gmail.com>
2023-12-04 16:17:26 +0700

    Update SignInActivity.kt

--- app/src/main/java/com/capstone/bloomy/ui/activity/SignInActivity.kt
+++ app/src/main/java/com/capstone/bloomy/ui/activity/SignInActivity.kt
@@ -3,7 +3,6 @@ package com.capstone.bloomy.ui.activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.capstone.bloomy.R
 import com.capstone.bloomy.databinding.ActivitySignInBinding
 
 class SignInActivity : AppCompatActivity() {
@@ -16,15 +15,14 @@ class SignInActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.btnSignIn.setOnClickListener {
-            val dashboardIntent = Intent(this, DashboardActivity::class.java)
-            startActivity(dashboardIntent)
+            val mainIntent = Intent(this, MainActivity::class.java)
+            startActivity(mainIntent)
             finish()
         }
 
         binding.btnSignUp.setOnClickListener {
             val signUpIntent = Intent(this, SignUpActivity::class.java)
             startActivity(signUpIntent)
-            finish()
         }
     }
 }
