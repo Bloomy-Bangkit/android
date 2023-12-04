d7f6bb99ae6c00cd3c3fe1c8bd129549bcbb3ed1
Sandhi Karunia Sugihartana <sandhi372@gmail.com>
2023-12-04 16:16:56 +0700

    Update NewsDetailActivity.kt

--- app/src/main/java/com/capstone/bloomy/ui/activity/NewsDetailActivity.kt
+++ app/src/main/java/com/capstone/bloomy/ui/activity/NewsDetailActivity.kt
@@ -7,6 +7,7 @@ import android.os.Bundle
 import android.view.View
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import com.capstone.bloomy.data.model.TodayNewsModel
 import com.capstone.bloomy.data.model.TopNewsModel
 import com.capstone.bloomy.databinding.ActivityNewsDetailBinding
 
@@ -21,6 +22,7 @@ class NewsDetailActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         @Suppress("DEPRECATION") val selectedTopNews = intent.getParcelableExtra<TopNewsModel>("selected_top_news")
+        @Suppress("DEPRECATION") val selectedTodayNews = intent.getParcelableExtra<TodayNewsModel>("selected_today_news")
 
         if (selectedTopNews != null) {
             binding.webViewNewsDetail.settings.javaScriptEnabled = true
@@ -40,6 +42,24 @@ class NewsDetailActivity : AppCompatActivity() {
             }
 
             binding.webViewNewsDetail.loadUrl(selectedTopNews.webUrl)
+        } else if (selectedTodayNews != null) {
+            binding.webViewNewsDetail.settings.javaScriptEnabled = true
+
+            binding.webViewNewsDetail.webViewClient = object : WebViewClient() {
+                override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
+                    super.onPageStarted(view, url, favicon)
+                    binding.progressBarNewsDetail.visibility = View.VISIBLE
+                    binding.webViewNewsDetail.visibility = View.INVISIBLE
+                }
+
+                override fun onPageFinished(view: WebView?, url: String?) {
+                    super.onPageFinished(view, url)
+                    binding.progressBarNewsDetail.visibility = View.GONE
+                    binding.webViewNewsDetail.visibility = View.VISIBLE
+                }
+            }
+
+            binding.webViewNewsDetail.loadUrl(selectedTodayNews.webUrl)
         }
     }
 }
