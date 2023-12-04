1b793a9869aa4af064820f8e17e5ca180ba9e2a8
Sandhi Karunia Sugihartana <sandhi372@gmail.com>
2023-12-04 16:17:37 +0700

    Update SignUpSuccessActivity.kt

--- app/src/main/java/com/capstone/bloomy/ui/activity/SignUpSuccessActivity.kt
+++ app/src/main/java/com/capstone/bloomy/ui/activity/SignUpSuccessActivity.kt
@@ -3,7 +3,6 @@ package com.capstone.bloomy.ui.activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.capstone.bloomy.R
 import com.capstone.bloomy.databinding.ActivitySignUpSuccessBinding
 
 class SignUpSuccessActivity : AppCompatActivity() {
