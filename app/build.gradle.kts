96fa3e283dc63a6ec533968424692bada947f413
Sandhi Karunia Sugihartana <sandhi372@gmail.com>
2023-12-11 19:43:13 +0700

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,6 +17,11 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
+        buildConfigField("String", "BASE_URL_INDONESIA", "\"https://dev.farizdotid.com/api/daerahindonesia/\"")
+        buildConfigField("String", "BASE_URL_MODEL", "\"https://api-model-vixypb3qiq-uc.a.run.app/\"")
+        buildConfigField("String", "BASE_URL_SERVICE", "\"https://api-service-vixypb3qiq-uc.a.run.app/\"")
+        buildConfigField("String", "BASE_URL_WEATHER", "\"https://api.openweathermap.org/data/2.5/\"")
     }
 
     buildTypes {
@@ -39,18 +44,30 @@ android {
     }
 
     buildFeatures {
+        buildConfig = true
         viewBinding = true
     }
 }
 
 dependencies {
 
+    implementation("androidx.activity:activity-ktx:1.8.1")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.datastore:datastore-preferences:1.0.0")
+    implementation("androidx.fragment:fragment-ktx:1.6.2")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.2")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
     implementation("de.hdodenhof:circleimageview:3.1.0")
     implementation("com.github.bumptech.glide:glide:4.16.0")
     implementation("com.google.android.material:material:1.10.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.11.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
 
     testImplementation("junit:junit:4.13.2")
 
