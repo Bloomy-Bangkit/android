bb24f06c43ab1f971f85aa8a781590539c92ce74
Sandhi Karunia Sugihartana <sandhi372@gmail.com>
2023-12-11 19:44:21 +0700

    Create ProfileResponse.kt

---
+++ app/src/main/java/com/capstone/bloomy/data/response/ProfileResponse.kt
@@ -0,0 +1,117 @@
+package com.capstone.bloomy.data.response
+
+import com.google.gson.annotations.SerializedName
+
+data class ProfileResponse(
+
+    @field:SerializedName("error")
+    val error: Boolean,
+
+    @field:SerializedName("message")
+    val message: String,
+
+    @field:SerializedName("data")
+    val profileData: ProfileData
+)
+
+data class ProfileData(
+
+    @field:SerializedName("email")
+    val email: String,
+
+    @field:SerializedName("username")
+    val username: String,
+
+    @field:SerializedName("activated")
+    val activated: Boolean,
+
+    @field:SerializedName("nama")
+    val nama: String,
+
+    @field:SerializedName("nohp")
+    val nohp: String,
+
+    @field:SerializedName("alamat")
+    val alamat: String,
+
+    @field:SerializedName("kota")
+    val kota: String,
+
+    @field:SerializedName("photo")
+    val photo: String,
+
+    @field:SerializedName("description")
+    val description: String
+)
+
+data class EditProfileResponse(
+
+    @field:SerializedName("error")
+    val error: Boolean,
+
+    @field:SerializedName("message")
+    val message: String,
+
+    @field:SerializedName("data")
+    val editProfileData: EditProfileData
+)
+
+data class EditProfileData(
+
+    @field:SerializedName("username")
+    val username: String,
+
+    @field:SerializedName("actived")
+    val actived: Boolean,
+
+    @field:SerializedName("nama")
+    val nama: String,
+
+    @field:SerializedName("alamat")
+    val alamat: String,
+
+    @field:SerializedName("kota")
+    val kota: String,
+
+    @field:SerializedName("description")
+    val description: String
+)
+
+data class EditPhotoProfileResponse(
+
+    @field:SerializedName("error")
+    val error: Boolean,
+
+    @field:SerializedName("message")
+    val message: String,
+
+    @field:SerializedName("data")
+    val editPhotoProfileData: EditPhotoProfileData
+)
+
+data class EditPhotoProfileData(
+
+    @field:SerializedName("username")
+    val username: String,
+
+    @field:SerializedName("photo")
+    val photo: String
+)
+
+data class ResetPasswordResponse(
+
+    @field:SerializedName("error")
+    val error: Boolean,
+
+    @field:SerializedName("message")
+    val message: String,
+
+    @field:SerializedName("data")
+    val resetPasswordData: ResetPasswordData
+)
+
+data class ResetPasswordData(
+
+    @field:SerializedName("username")
+    val username: String
+)
