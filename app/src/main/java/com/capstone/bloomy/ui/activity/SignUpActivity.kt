d4d93a7cbcd23ad14bda15f17edf836c575bc76e
Sandhi Karunia Sugihartana <sandhi372@gmail.com>
2023-12-13 14:24:36 +0700

    Update SignUpActivity.kt

--- app/src/main/java/com/capstone/bloomy/ui/activity/SignUpActivity.kt
+++ app/src/main/java/com/capstone/bloomy/ui/activity/SignUpActivity.kt
@@ -1,8 +1,12 @@
 package com.capstone.bloomy.ui.activity
 
+import android.app.Dialog
 import android.content.Intent
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
 import android.widget.Button
 import android.widget.Toast
 import androidx.activity.viewModels
@@ -24,35 +28,13 @@ class SignUpActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.btnSignUp.setOnClickListener {
-            val signUp = binding.btnSignUp
             val username = binding.etUsernameSignUp.text.toString()
             val email = binding.etEmailSignUp.text.toString()
             val password = binding.etPasswordSignUp.text.toString()
             val confirmPassword = binding.etConfirmPasswordSignUp.text.toString()
 
             if (email.isNotEmpty() && username.isNotEmpty() && password.isNotEmpty() && confirmPassword.isNotEmpty()) {
-                showLoading(signUp, true)
-
-                authenticationViewModel.signUp(email, username, password, confirmPassword)
-                authenticationViewModel.signUpResponse.observe(this@SignUpActivity) { response ->
-                    val error = response?.error
-                    val message = response?.message.toString()
-
-                    if (error != null) {
-                        showLoading(signUp, false)
-                        if (error == true) {
-                            Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
-                            authenticationViewModel.defaultSignUp()
-                        } else {
-                            Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
-                            authenticationViewModel.defaultSignUp()
-
-                            val signUpSuccessIntent = Intent(this, SignUpSuccessActivity::class.java)
-                            signUpSuccessIntent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
-                            startActivity(signUpSuccessIntent)
-                        }
-                    }
-                }
+                showSignUpDialog()
             } else {
                 Toast.makeText(this, getString(R.string.invalid_input), Toast.LENGTH_SHORT).show()
             }
@@ -61,6 +43,55 @@ class SignUpActivity : AppCompatActivity() {
         binding.btnSignIn.setOnClickListener {
             startActivity(Intent(this, SignInActivity::class.java))
         }
+    }
+
+    private fun showSignUpDialog() {
+        val dialog = Dialog(this)
+
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        dialog.setCancelable(false)
+        dialog.setContentView(R.layout.sign_up_dialog)
+        dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+
+        val btnNoSignUp: Button = dialog.findViewById(R.id.btn_no_sign_up_dialog)
+        val btnYesSignUp: Button = dialog.findViewById(R.id.btn_yes_sign_up_dialog)
+
+        btnYesSignUp.setOnClickListener {
+            val signUp = binding.btnSignUp
+            val username = binding.etUsernameSignUp.text.toString()
+            val email = binding.etEmailSignUp.text.toString()
+            val password = binding.etPasswordSignUp.text.toString()
+            val confirmPassword = binding.etConfirmPasswordSignUp.text.toString()
+
+            showLoading(signUp, true)
+
+            authenticationViewModel.signUp(email, username, password, confirmPassword)
+            authenticationViewModel.signUpResponse.observe(this@SignUpActivity) { response ->
+                val error = response?.error
+                val message = response?.message.toString()
+
+                if (error != null) {
+                    showLoading(signUp, false)
+                    if (error == true) {
+                        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+                        authenticationViewModel.defaultSignUp()
+                    } else {
+                        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+                        authenticationViewModel.defaultSignUp()
+
+                        val signUpSuccessIntent = Intent(this, SignUpSuccessActivity::class.java)
+                        signUpSuccessIntent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+                        startActivity(signUpSuccessIntent)
+                    }
+                }
+            }
+        }
+
+        btnNoSignUp.setOnClickListener {
+            dialog.dismiss()
+        }
+
+        dialog.show()
     }
 
     private fun showLoading(signUp: Button, isLoading: Boolean) { signUp.text = if (!isLoading) getString(R.string.btn_sign_up) else getString(R.string.btn_loading) }
