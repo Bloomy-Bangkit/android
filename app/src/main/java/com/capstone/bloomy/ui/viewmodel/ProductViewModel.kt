71eef1201c7729d3cd245af1b86f6dcdaa233a05
Sandhi Karunia Sugihartana <sandhi372@gmail.com>
2023-12-14 19:10:31 +0700

    Update ProductViewModel.kt

--- app/src/main/java/com/capstone/bloomy/ui/viewmodel/ProductViewModel.kt
+++ app/src/main/java/com/capstone/bloomy/ui/viewmodel/ProductViewModel.kt
@@ -6,8 +6,10 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.capstone.bloomy.data.response.DeleteProductData
 import com.capstone.bloomy.data.response.EditProfileResponse
+import com.capstone.bloomy.data.response.ProductByGradeData
 import com.capstone.bloomy.data.response.ProductByIdData
 import com.capstone.bloomy.data.response.ProductByUsernameData
+import com.capstone.bloomy.data.response.ProductData
 import com.capstone.bloomy.repository.ProductRepository
 import kotlinx.coroutines.launch
 import retrofit2.HttpException
@@ -15,13 +17,31 @@ import java.io.File
 
 class ProductViewModel(private val productRepository: ProductRepository) : ViewModel() {
 
+    private val _products = MutableLiveData<List<ProductData>>()
+    val products: LiveData<List<ProductData>> = _products
+
     private val _product = MutableLiveData<List<ProductByUsernameData>>()
     val product: LiveData<List<ProductByUsernameData>> = _product
 
     private val _detailProduct = MutableLiveData<ProductByIdData>()
     val detailProduct: LiveData<ProductByIdData> = _detailProduct
 
+    private val _productByGrade = MutableLiveData<List<ProductByGradeData>>()
+    val productByGrade: LiveData<List<ProductByGradeData>> = _productByGrade
+
     private val _responseCode = MutableLiveData<Int>()
+
+    fun getProduct() {
+        viewModelScope.launch {
+            try {
+                val response = productRepository.getProduct()
+
+                if (response.isSuccessful) _products.value = response.body()?.productData else _responseCode.value = response.code()
+            } catch (e: HttpException) {
+                _responseCode.value = e.code()
+            }
+        }
+    }
 
     fun getProductByUsername(username: String) {
         viewModelScope.launch {
@@ -46,6 +66,19 @@ class ProductViewModel(private val productRepository: ProductRepository) : ViewM
             }
         }
     }
+
+    fun getProductByGrade(grade: String) {
+        viewModelScope.launch {
+            try {
+                val response = productRepository.getProductByGrade(grade)
+
+                if (response.isSuccessful) _productByGrade.value = response.body()?.productByGradeData else _responseCode.value = response.code()
+            } catch (e: HttpException) {
+                _responseCode.value = e.code()
+            }
+        }
+    }
+
 
     fun addProduct(file: File, nama: String, grade: String, price: Number, weight: Number, description: String) = productRepository.addProduct(file, nama, grade, price, weight, description)
 
