8221ecc6ba885c2cac3367a14ef2c3d7b375a65e
Sandhi Karunia Sugihartana <sandhi372@gmail.com>
2023-12-14 19:09:33 +0700

    Update MarketSellerDetailActivity.kt

--- app/src/main/java/com/capstone/bloomy/ui/activity/MarketSellerDetailActivity.kt
+++ app/src/main/java/com/capstone/bloomy/ui/activity/MarketSellerDetailActivity.kt
@@ -1,19 +1,26 @@
 package com.capstone.bloomy.ui.activity
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.recyclerview.widget.GridLayoutManager
+import com.bumptech.glide.Glide
 import com.capstone.bloomy.R
-import com.capstone.bloomy.data.model.ProductModel
+import com.capstone.bloomy.data.response.ProductByUsernameData
+import com.capstone.bloomy.data.response.ProfileByUsernameData
 import com.capstone.bloomy.databinding.ActivityMarketSellerDetailBinding
 import com.capstone.bloomy.ui.adapter.ProductSellerDetailAdapter
+import com.capstone.bloomy.ui.viewmodel.ProductViewModel
+import com.capstone.bloomy.ui.viewmodel.ProfileViewModel
+import com.capstone.bloomy.ui.viewmodelfactory.ProductViewModelFactory
+import com.capstone.bloomy.ui.viewmodelfactory.ProfileViewModelFactory
 
 class MarketSellerDetailActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMarketSellerDetailBinding
 
-    private val productSellerDetailList = ArrayList<ProductModel>()
+    private val profileViewModelFactory: ProfileViewModelFactory = ProfileViewModelFactory.getInstance(this@MarketSellerDetailActivity)
+    private val profileViewModel: ProfileViewModel by viewModels { profileViewModelFactory }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,45 +30,47 @@ class MarketSellerDetailActivity : AppCompatActivity() {
         setSupportActionBar(binding.materialToolBarSellerDetail)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
-        binding.recyclerViewProductSellerDetail.setHasFixedSize(true)
+        val extraSellerUsername = intent.getStringExtra("seller_username")
 
-        productSellerDetailList.addAll(getListProductSellerDetail())
-        showProductSellerDetailList()
-    }
+        profileViewModel.getProfileByUsername(extraSellerUsername.toString())
+        profileViewModel.profileByUsername.observe(this) { profile ->
+            setProfileByUsername(profile)
 
-    private fun getListProductSellerDetail(): ArrayList<ProductModel> {
-        val dataTitleProductSellerDetail = resources.getStringArray(R.array.data_title_product_seller_detail)
-        val dataImageUrlProductSellerDetail = resources.getStringArray(R.array.data_image_url_product_seller_detail)
-        val dataGradeProductSellerDetail = resources.getStringArray(R.array.data_grade_product_seller_detail)
-        val dataPriceProductSellerDetail = resources.getStringArray(R.array.data_price_product_seller_detail)
-        val dataRatingProductSellerDetail = resources.getStringArray(R.array.data_rating_product_seller_detail)
-        val dataSoldProductSellerDetail = resources.getStringArray(R.array.data_sold_product_seller_detail)
-        val dataLocationProductSellerDetail = resources.getStringArray(R.array.data_location_product_seller_detail)
-        val dataIsFavoriteProductSellerDetail = resources.getStringArray(R.array.data_is_favorite_product_seller_detail)
-        val productSellerDetailList = ArrayList<ProductModel>()
+            val profileByUsernameData: ProfileByUsernameData = profile ?: return@observe
 
-        val minLength = minOf(dataTitleProductSellerDetail.size, dataImageUrlProductSellerDetail.size, dataGradeProductSellerDetail.size, dataPriceProductSellerDetail.size, dataRatingProductSellerDetail.size, dataSoldProductSellerDetail.size, dataLocationProductSellerDetail.size, dataIsFavoriteProductSellerDetail.size)
+            val productViewModelFactory: ProductViewModelFactory = ProductViewModelFactory.getInstance(this@MarketSellerDetailActivity)
+            val productViewModel: ProductViewModel by viewModels { productViewModelFactory }
 
-        for (i in 0 until minLength) {
-            val productShop = ProductModel(dataTitleProductSellerDetail[i], dataImageUrlProductSellerDetail[i], dataGradeProductSellerDetail[i], dataPriceProductSellerDetail[i], dataRatingProductSellerDetail[i], dataSoldProductSellerDetail[i], dataLocationProductSellerDetail[i], dataIsFavoriteProductSellerDetail[i])
-            productSellerDetailList.add(productShop)
-        }
-
-        return productSellerDetailList
-    }
-
-    private fun showProductSellerDetailList() {
-        binding.recyclerViewProductSellerDetail.layoutManager = GridLayoutManager(this, 2)
-
-        val productSellerDetailAdapter = ProductSellerDetailAdapter(productSellerDetailList)
-
-        binding.recyclerViewProductSellerDetail.adapter = productSellerDetailAdapter
-
-        productSellerDetailAdapter.setOnItemClickCallback(object : ProductSellerDetailAdapter.OnItemClickCallback {
-            override fun onItemClicked(product: ProductModel) {
-                val sellerProductDetailIntent = Intent(this@MarketSellerDetailActivity, MarketProductDetailActivity::class.java)
-                startActivity(sellerProductDetailIntent)
+            productViewModel.getProductByUsername(profileByUsernameData.username)
+            productViewModel.product.observe(this) { product ->
+                setProductByUsernameData(product)
             }
-        })
+        }
+    }
+
+    private fun setProfileByUsername(profileByUsername: ProfileByUsernameData) {
+        with(binding) {
+            Glide.with(this@MarketSellerDetailActivity)
+                .load(profileByUsername.photo)
+                .into(imgSellerDetail)
+
+            tvNameSellerDetail.text = profileByUsername.nama
+            tvPhoneSellerDetail.text = profileByUsername.nohp
+            tvLocationSellerDetail.text = profileByUsername.kota
+        }
+    }
+
+    private fun setProductByUsernameData(product: List<ProductByUsernameData>) {
+        val layoutManager = GridLayoutManager(this, 2)
+        binding.recyclerViewProductSellerDetail.layoutManager = layoutManager
+
+        val adapter = ProductSellerDetailAdapter()
+        adapter.submitList(product)
+        binding.recyclerViewProductSellerDetail.adapter = adapter
+
+        val itemCount = adapter.itemCount
+        if (itemCount != 0) {
+            binding.tvProductSellerDetail.text = "$itemCount Product"
+        }
     }
 }
