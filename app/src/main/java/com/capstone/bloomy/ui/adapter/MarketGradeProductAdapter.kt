eb957453bef39fa5faa31aabc53ea803a5e6f3d0
Sandhi Karunia Sugihartana <sandhi372@gmail.com>
2023-12-14 19:09:57 +0700

    Update MarketGradeProductAdapter.kt

--- app/src/main/java/com/capstone/bloomy/ui/adapter/MarketGradeProductAdapter.kt
+++ app/src/main/java/com/capstone/bloomy/ui/adapter/MarketGradeProductAdapter.kt
@@ -1,17 +1,17 @@
 package com.capstone.bloomy.ui.adapter
 
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
-import com.capstone.bloomy.data.model.ProductModel
+import com.capstone.bloomy.data.response.ProductByGradeData
 import com.capstone.bloomy.databinding.ItemRowMarketGradeProductBinding
+import com.capstone.bloomy.ui.activity.MarketProductDetailActivity
 
-class MarketGradeProductAdapter(private val marketGradeProduct: ArrayList<ProductModel>) : RecyclerView.Adapter<MarketGradeProductAdapter.MyViewHolder>() {
-
-    private lateinit var onItemClickCallback: OnItemClickCallback
-
-    class MyViewHolder(var binding: ItemRowMarketGradeProductBinding) : RecyclerView.ViewHolder(binding.root)
+class MarketGradeProductAdapter : ListAdapter<ProductByGradeData, MarketGradeProductAdapter.MyViewHolder>(DIFF_CALLBACK) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
         val binding = ItemRowMarketGradeProductBinding.inflate(LayoutInflater.from(parent.context), parent, false)
@@ -20,32 +20,50 @@ class MarketGradeProductAdapter(private val marketGradeProduct: ArrayList<Produc
     }
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-        val (title, imageUrl, _, price, rating, sold, location, _) = marketGradeProduct[position]
+        val product = getItem(position)
 
-        Glide.with(holder.itemView.context)
-            .load(imageUrl)
-            .into(holder.binding.imgMarketGradeProduct)
+        holder.bind(product)
+        holder.itemView.setOnClickListener {
+            val detailProduct = Intent(holder.itemView.context, MarketProductDetailActivity::class.java)
 
-        holder.binding.tvTitleMarketGradeProduct.text = title
+            MarketProductDetailActivity.PRODUCT_ID = product.idProduct
+            MarketProductDetailActivity.SELLER_USERNAME = product.usernameSeller
 
-        holder.binding.tvPriceMarketGradeProduct.text = price
-
-        holder.binding.tvRatingMarketGradeProduct.text = rating
-
-        holder.binding.tvSoldMarketGradeProduct.text = sold
-
-        holder.binding.tvLocationMarketGradeProduct.text = location
-
-        holder.itemView.setOnClickListener { onItemClickCallback.onItemClicked(marketGradeProduct[holder.adapterPosition]) }
+            holder.itemView.context.startActivity(detailProduct)
+        }
     }
 
-    override fun getItemCount(): Int = marketGradeProduct.size
+    class MyViewHolder(private val binding: ItemRowMarketGradeProductBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(productByGradeData: ProductByGradeData) {
+            Glide.with(binding.imgMarketGradeProduct)
+                .load(productByGradeData.picture)
+                .into(binding.imgMarketGradeProduct)
 
-    fun setOnItemClickCallback(onItemClickCallback: OnItemClickCallback) {
-        this.onItemClickCallback = onItemClickCallback
+            binding.tvTitleMarketGradeProduct.text = productByGradeData.nama
+            binding.tvPriceMarketGradeProduct.text = formatCurrency(productByGradeData.price)
+            binding.tvQuantityMarketGradeProduct.text = formatWeight(productByGradeData.weight)
+            binding.tvLocationMarketGradeProduct.text = productByGradeData.kota
+        }
     }
 
-    interface OnItemClickCallback {
-        fun onItemClicked(marketGradeProduct: ProductModel)
+    companion object {
+        val DIFF_CALLBACK = object : DiffUtil.ItemCallback<ProductByGradeData>() {
+            override fun areItemsTheSame(oldItem: ProductByGradeData, newItem: ProductByGradeData): Boolean {
+                return oldItem == newItem
+            }
+
+            override fun areContentsTheSame(oldItem: ProductByGradeData, newItem: ProductByGradeData): Boolean {
+                return oldItem == newItem
+            }
+        }
+
+        fun formatCurrency(amount: Int): String {
+            val formattedAmount = String.format("Rp%,d", amount)
+            return formattedAmount.replace(',', '.')
+        }
+
+        fun formatWeight(weight: Int): String {
+            return "$weight kg left"
+        }
     }
 }
