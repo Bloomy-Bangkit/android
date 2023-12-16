b46f6c8c9f2042e4ae5d8a6af95e099bbd437fae
Sandhi Karunia Sugihartana <sandhi372@gmail.com>
2023-12-16 10:52:47 +0700

    Create FishPricingViewModelFactory.kt

---
+++ app/src/main/java/com/capstone/bloomy/ui/viewmodelfactory/FishPricingViewModelFactory.kt
@@ -0,0 +1,28 @@
+package com.capstone.bloomy.ui.viewmodelfactory
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.capstone.bloomy.di.FishPricingInjection
+import com.capstone.bloomy.repository.FishPricingRepository
+import com.capstone.bloomy.ui.viewmodel.FishPricingViewModel
+
+class FishPricingViewModelFactory private constructor(private val fishPricingRepository: FishPricingRepository) : ViewModelProvider.NewInstanceFactory() {
+
+    @Suppress("UNCHECKED_CAST")
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(FishPricingViewModel::class.java)) {
+            return FishPricingViewModel(fishPricingRepository) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class: ${modelClass.name}")
+    }
+
+    companion object {
+        @Volatile
+        private var INSTANCE: FishPricingViewModelFactory? = null
+
+        fun getInstance(): FishPricingViewModelFactory =
+            INSTANCE ?: synchronized(this) {
+                INSTANCE ?: FishPricingViewModelFactory(FishPricingInjection.provideRepository())
+            }.also { INSTANCE = it }
+    }
+}
