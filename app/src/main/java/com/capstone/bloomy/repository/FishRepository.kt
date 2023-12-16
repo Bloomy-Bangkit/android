84032a66b68efb2505014cbb4571e8608456b571
Sandhi Karunia Sugihartana <sandhi372@gmail.com>
2023-12-16 10:51:51 +0700

    Create FishRepository.kt

---
+++ app/src/main/java/com/capstone/bloomy/repository/FishRepository.kt
@@ -0,0 +1,22 @@
+package com.capstone.bloomy.repository
+
+import com.capstone.bloomy.data.remote.fish.FishService
+
+class FishRepository private constructor(private val fishService: FishService) {
+
+    suspend fun getFish() = fishService.getFish()
+
+    suspend fun getFishById(id: String) = fishService.getFishById(id)
+
+    companion object {
+        @Volatile
+        private var INSTANCE: FishRepository? = null
+
+        fun getInstance(
+            fishService: FishService
+        ): FishRepository =
+            INSTANCE ?: synchronized(this) {
+                INSTANCE ?: FishRepository(fishService)
+            }.also { INSTANCE = it }
+    }
+}
