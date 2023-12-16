3fe4d4e5d78e7d0869f0086076572e508736d457
Sandhi Karunia Sugihartana <sandhi372@gmail.com>
2023-12-16 10:51:15 +0700

    Create FishResponse.kt

---
+++ app/src/main/java/com/capstone/bloomy/data/response/FishResponse.kt
@@ -0,0 +1,63 @@
+package com.capstone.bloomy.data.response
+
+import com.google.gson.annotations.SerializedName
+
+data class FishResponse(
+
+    @field:SerializedName("error")
+    val error: Boolean,
+
+    @field:SerializedName("message")
+    val message: String,
+
+    @field:SerializedName("data")
+    val fishData: List<FishData>
+)
+
+data class FishData(
+
+    @field:SerializedName("idFish")
+    val idFish: String,
+
+    @field:SerializedName("nama")
+    val nama: String,
+
+    @field:SerializedName("price")
+    val price: Int,
+
+    @field:SerializedName("createdAt")
+    val createdAt: String,
+
+    @field:SerializedName("updatedAt")
+    val updatedAt: String
+)
+
+data class FishByIdResponse(
+
+    @field:SerializedName("error")
+    val error: Boolean,
+
+    @field:SerializedName("message")
+    val message: String,
+
+    @field:SerializedName("data")
+    val fishByIdData: FishByIdData
+)
+
+data class FishByIdData(
+
+    @field:SerializedName("idFish")
+    val idFish: String,
+
+    @field:SerializedName("nama")
+    val nama: String,
+
+    @field:SerializedName("price")
+    val price: Int,
+
+    @field:SerializedName("createdAt")
+    val createdAt: String,
+
+    @field:SerializedName("updatedAt")
+    val updatedAt: String
+)
