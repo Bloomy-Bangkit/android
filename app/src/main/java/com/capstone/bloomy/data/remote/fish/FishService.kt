7a966477815ab03ae28cf5447133711c2730abb1
Sandhi Karunia Sugihartana <sandhi372@gmail.com>
2023-12-20 04:15:07 +0700

    Update FishService.kt

--- app/src/main/java/com/capstone/bloomy/data/remote/fish/FishService.kt
+++ app/src/main/java/com/capstone/bloomy/data/remote/fish/FishService.kt
@@ -1,18 +1,11 @@
 package com.capstone.bloomy.data.remote.fish
 
-import com.capstone.bloomy.data.response.FishByIdResponse
 import com.capstone.bloomy.data.response.FishResponse
 import retrofit2.Response
 import retrofit2.http.GET
-import retrofit2.http.Path
 
 interface FishService {
 
     @GET("fishs")
     suspend fun getFish(): Response<FishResponse>
-
-    @GET("fish/{id}")
-    suspend fun getFishById(
-        @Path("id") id: String
-    ): Response<FishByIdResponse>
 }
