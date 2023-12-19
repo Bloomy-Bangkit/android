167cd070ea5c0d5a140ffb3c768c1ae95e29db80
Sandhi Karunia Sugihartana <sandhi372@gmail.com>
2023-12-20 04:19:14 +0700

    Update FishViewModel.kt

--- app/src/main/java/com/capstone/bloomy/ui/viewmodel/FishViewModel.kt
+++ app/src/main/java/com/capstone/bloomy/ui/viewmodel/FishViewModel.kt
@@ -4,7 +4,6 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.capstone.bloomy.data.response.FishByIdData
 import com.capstone.bloomy.data.response.FishData
 import com.capstone.bloomy.repository.FishRepository
 import kotlinx.coroutines.launch
@@ -15,9 +14,6 @@ class FishViewModel(private val fishRepository: FishRepository) : ViewModel() {
     private val _fish = MutableLiveData<List<FishData>>()
     val fish: LiveData<List<FishData>> = _fish
 
-    private val _fishById = MutableLiveData<FishByIdData>()
-    val fishById: LiveData<FishByIdData> = _fishById
-
     private val _responseCode = MutableLiveData<Int>()
 
     fun getFish() {
@@ -26,18 +22,6 @@ class FishViewModel(private val fishRepository: FishRepository) : ViewModel() {
                 val response = fishRepository.getFish()
 
                 if (response.isSuccessful) _fish.value = response.body()?.fishData else _responseCode.value = response.code()
-            } catch (e: HttpException) {
-                _responseCode.value = e.code()
-            }
-        }
-    }
-
-    fun getFishById(id: String) {
-        viewModelScope.launch {
-            try {
-                val response = fishRepository.getFishById(id)
-
-                if (response.isSuccessful) _fishById.value = response.body()?.fishByIdData else _responseCode.value = response.code()
             } catch (e: HttpException) {
                 _responseCode.value = e.code()
             }
