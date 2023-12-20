c9736b938eae0d2ce8a96ee1bbe5e0c9714d3ba7
Sandhi Karunia Sugihartana <sandhi372@gmail.com>
2023-12-20 18:33:02 +0700

    Update LocationResponse.kt

--- app/src/main/java/com/capstone/bloomy/data/response/LocationResponse.kt
+++ app/src/main/java/com/capstone/bloomy/data/response/LocationResponse.kt
@@ -2,20 +2,47 @@ package com.capstone.bloomy.data.response
 
 import com.google.gson.annotations.SerializedName
 
-data class ProvinsiTestResponse(
+data class ProvinsiResponse(
 
     @field:SerializedName("error")
     val error: Boolean,
 
     @field:SerializedName("message")
-    val message: String
+    val message: String,
+
+    @field:SerializedName("data")
+    val provinsiData: List<ProvinsiData>
 )
 
-data class KotaTestResponse(
+data class ProvinsiData(
+
+    @field:SerializedName("id")
+    val id: Int,
+
+    @field: SerializedName("nama")
+    val nama: String
+)
+
+data class KotaResponse(
 
     @field:SerializedName("error")
     val error: Boolean,
 
     @field:SerializedName("message")
-    val message: String
+    val message: String,
+
+    @field:SerializedName("data")
+    val kotaData: List<KotaData>
+)
+
+data class KotaData(
+
+    @field:SerializedName("id")
+    val id: Int,
+
+    @field:SerializedName("id_provinsi")
+    val idProvinsi: Int,
+
+    @field:SerializedName("nama")
+    val nama: String
 )
