66de6027acb239f17356cc1eabcd40edd7b1877c
Sandhi Karunia Sugihartana <sandhi372@gmail.com>
2023-12-20 18:34:37 +0700

    Update TransactionActivity.kt

--- app/src/main/java/com/capstone/bloomy/ui/activity/TransactionActivity.kt
+++ app/src/main/java/com/capstone/bloomy/ui/activity/TransactionActivity.kt
@@ -1,7 +1,10 @@
 package com.capstone.bloomy.ui.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.activity.OnBackPressedCallback
 import androidx.viewpager2.widget.ViewPager2
 import com.capstone.bloomy.R
 import com.capstone.bloomy.databinding.ActivityTransactionBinding
@@ -32,6 +35,30 @@ class TransactionActivity : AppCompatActivity() {
         TabLayoutMediator(tabLayout, viewPager) { tab, position ->
             tab.text = resources.getString(TAB_TITLES[position])
         }.attach()
+
+        onBackPressedDispatcher.addCallback(this, object: OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                navigateToMainActivity()
+            }
+        })
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                navigateToMainActivity()
+                true
+            }
+
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun navigateToMainActivity() {
+        val intent = Intent(this, MainActivity::class.java)
+        intent.putExtra("navigateToProfileFragment", true)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK)
+        startActivity(intent)
     }
 
     companion object {
